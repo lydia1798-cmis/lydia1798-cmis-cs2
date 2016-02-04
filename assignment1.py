29c6ecac95b49878c42c728706713d7389cc621c
cmis <cmis@cmis-hs21.(none)>
2016-02-04 14:35:59 +0700

    I finished the assignment and figured out the new things that I had to do.

--- assignment1.py
+++ assignment1.py
@@ -1,11 +1,16 @@
-myName = "Lydia"
-myAge = 14.7
-myHeight = 1.6764
-squareSide = 4
-lengthRectanlge = 6
-heightRectangle = 4
-ageinmonths = myAge * 12
-yearsleft = 78.74 - myAge
-heightinft = myHeight * 3.28084
-heightdifference = 1.587 - myHeight
-squareArea = squareSide * 2
+myName = "Lydia" #variable assigned to my name
+myAge = 14.7 #variable assigned to my age with a decimal
+myHeight = 1.6764 #variable assigned to my height in meters
+squareSide = 4 #a random value I created to be the side of a aquare
+lengthRectangle = 6 #a random value I created to be the length of a rectangle
+heightRectangle = 4 #a random valuse I created to be the height of a reactangle
+ageinmonths = myAge * 12 #my age in months
+yearsleft = 78.74 - myAge #how many years I have left according average lifespan
+heightinft = myHeight * 3.28084 #my height in feet
+heightdifference = 1.587 - myHeight #difference of height between me and the average girl
+squareArea = squareSide * squareSide #the area of the square that I created
+squareVolume = (squareSide * squareSide * squareSide) / 2 #half the volume of the square that I created
+areaRectangle = (lengthRectangle * heightRectangle) / 9 #1/9th of the area of the rectanlge that I created
+print "I'm " + str(myName) + ". I am " + str(myAge) + " years old! I am also " + str(ageinmonths) + " months old! According to the internet I have " + str(yearsleft) + " years left to live. Apparently I am " + str(heightdifference) + " taller than most people my age!!! YAY!" #a random and sarastic message using 5 variables and +
+print "I have made a square with a side of " ,squareSide, " inches. The same square has an area of " ,squareArea, " inches as well as a half a volume of " ,squareVolume, " inches. I made a rectangle with a height of " ,heightRectangle, " inches and a length of " ,lengthRectangle, " inches." #a message using 5 more variables and a comma
+print ";)" * 10000 #code to print out 10000 winky faces in one step
