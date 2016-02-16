9bc993b7ea4698868880d0f9abcbe2201820ce2b
cmis <cmis@cmis-hs20.(none)>
2016-02-16 14:35:52 +0700

    still a lot more to do

--- functions.py
+++ functions.py
@@ -12,10 +12,10 @@ b = sub(9.1, 8)
 c = mul(9.1, 8)
 d = div(9.1, 8)
 
-print a
-print b
-print c
-print d
+print "adding - " + str(a)
+print "subtracting - " + str(b)
+print "multiplying - " + str(c)
+print "dividing - " + str(d)
 
 
 def hours_from_seconds(seconds):
@@ -23,18 +23,36 @@ def hours_from_seconds(seconds):
 	return hours
 
 hours = hours_from_seconds(86400)
-print hours
+print "hours in sconds - " + str(hours)
 
 import math
 
 def circle_area(radius):
-	diameter = radius * 2
-	area = diameter * math.pi
+	r_squared = radius * radius
+	area = math.pi * r_squared
 	return area
 
 circlearea = circle_area(5)
-print circlearea
+print "area of a circle - " + str(circlearea)
 
+def sphere_volume(radius):
+	r_cubed = radius * radius * radius
+	fraction = float(4/3.0) 
+	volume = fraction * math.pi * r_cubed
+	return volume
 
-NOT RIGHT
+spherevolume = sphere_volume(5)
+print "sphere volume - " + str(spherevolume) 
+
+def avg_volume(x, y):
+	a = x / 2
+	b = y / 2
+	a_volume = sphere_volume(a)
+	b_volume = sphere_volume(b)
+	together = a_volume + b_volume
+	avg = together / 2.0
+	return avg
+
+avg_ = avg_volume(10, 20)
+print "average sphere volume - " + str(avg_)
 
