fd1b5125cb6bb667145c8e17d9ca52e252d3e01e
cmis <cmis@cmis-hs20.(none)>
2016-02-17 13:35:34 +0700

    I am still working

---
+++ distance formula.py
@@ -0,0 +1,22 @@
+import math
+
+n = 2
+
+def sub(x_1, x_2):
+	a = x_1 - x_2
+	return a
+
+def sub(y_1, y_2):
+	b = y_1- y_2
+	return b
+
+def pow_(a):
+	math.pow(a, n)
+	return a**n
+
+def sqrt(b):
+	b_sqrt = math.sqrt(b)
+	return b_sqrt
+
+def sub(a_, b_)
+	
