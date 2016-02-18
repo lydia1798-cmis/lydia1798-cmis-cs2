57627e889d8aecdcd788f51e9c6baefd3bc455ff
cmis <cmis@cmis-hs20.(none)>
2016-02-18 09:52:29 +0700

    need to figure out the msg box asap

--- functions.py
+++ functions.py
@@ -23,7 +23,7 @@ def hours_from_seconds(seconds):
 	return hours
 
 hours = hours_from_seconds(86400)
-print "hours in sconds - " + str(hours)
+print "hours_from_seconds - " + str(hours)
 
 import math
 
@@ -33,7 +33,7 @@ def circle_area(radius):
 	return area
 
 circlearea = circle_area(5)
-print "area of a circle - " + str(circlearea)
+print "circle_area - " + str(circlearea)
 
 def sphere_volume(radius):
 	r_cubed = radius * radius * radius
@@ -42,7 +42,7 @@ def sphere_volume(radius):
 	return volume
 
 spherevolume = sphere_volume(5)
-print "sphere volume - " + str(spherevolume) 
+print "sphere_volume - " + str(spherevolume) 
 
 def avg_volume(x, y):
 	a = x / 2
@@ -54,5 +54,43 @@ def avg_volume(x, y):
 	return avg
 
 avg_ = avg_volume(10, 20)
-print "average sphere volume - " + str(avg_)
+print "avg_volume - " + str(avg_)
 
+def area(a, b, c):
+	add_s = a + b + c
+	s = 1/2.0 * add_s
+	sub_a = s - a
+	sub_b = s - b
+	sub_c = s - c
+	mul_eve = s * sub_a * sub_b * sub_c
+	sqrt = math.sqrt(mul_eve)
+	return sqrt
+
+area_ = area(1, 2, 2.5)
+print area_
+
+def right_align(string):
+	len_string = len(string)
+	sub = 80 - len_string
+	space = " "
+	space_ = space * sub
+	return space_ + string
+
+rightalign = right_align("Hello")
+print rightalign
+
+def center(string):
+	len_string = len(string)
+	div_str = len_string / 2
+	sub = 40 - div_str
+	space = " "
+	space_ = space * sub
+	return space_ + string
+	
+center_ = center("Hello")
+print center_
+
+def msg_box("string"):
+	len_string = len(string)
+	length = len_string + 4
+	
