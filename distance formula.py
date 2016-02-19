c9e47dc1d9a2a3d87465faeb8a2caaa9127d0f7c
cmis <cmis@cmis-hs20.(none)>
2016-02-19 13:51:08 +0700

    i finished

--- distance formula.py
+++ distance formula.py
@@ -2,13 +2,13 @@ import math
 
 n = 2
 
+def add(a, b):
+	c = a + b
+	return c
+
 def sub(x_1, x_2):
 	a = x_1 - x_2
 	return a
-
-def sub(y_1, y_2):
-	b = y_1- y_2
-	return b
 
 def pow_(a):
 	math.pow(a, n)
@@ -18,5 +18,22 @@ def sqrt(b):
 	b_sqrt = math.sqrt(b)
 	return b_sqrt
 
-def sub(a_, b_)
-	
+def output(c):
+	return "The distance between your two coordinate pairs: {}".format(c)
+
+def main():
+	x_1 = raw_input("Type the x in the first coordinate pair: ")
+	y_1 = raw_input("Type the y in the first coordinate pair: ")
+	x_2 = raw_input("Type the x in the second coordinate pair: ")
+	y_2 = raw_input("Type the y in the secind coordinate pair: ")
+	a = sub(float(x_1), float(x_2))
+	b = sub(float(y_1), float(y_2))
+	c = pow_(float(a))
+	d = pow_(float(b))
+	sum_ = add(float(c), float(d))
+	sqrt_ = sqrt(float(sum_))
+	print output(sqrt_)
+
+main()
+
+
