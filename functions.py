d4e2d1e82990b628a783344fe7e2472cd4739e98
cmis <cmis@cmis-hs20.(none)>
2016-02-23 08:41:18 +0700

    I am amost done with functins nad finished program

--- functions.py
+++ functions.py
@@ -90,7 +90,80 @@ def center(string):
 center_ = center("Hello")
 print center_
 
-def msg_box("string"):
+def msg_box(string):
+	string = string
 	len_string = len(string)
 	length = len_string + 4
+	dashed = "-" * length
+	return "+" + str(dashed) + "+ \n" + "|  " + str(string) + "  | \n" + "+" + str(dashed) + "+"
+
+box_hello = msg_box("Hello")
+print box_hello
 	
+box_cats = msg_box("I like cats!")
+print box_cats
+
+add = add(9.1, 8)
+print add
+add = add(89.9, 98)
+print add
+
+sub = sub(9.1, 8)
+print sub
+sub = sub(91, 86)
+print sub
+
+mul = mul(9.1, 8)
+print mul
+mul = mul(41, 85)
+print mul
+
+div = div(9.1, 8)
+print div
+div = div(1, 8.7)
+print div
+
+hours = hours_from_seconds(86400)
+print "hours_from_seconds - " + str(hours)
+hours = hours_from_seconds(565656565656565)
+print "hours_from_seconds - " + str(hours)
+
+circlearea = circle_area(5)
+print "circle_area - " + str(circlearea)
+circlearea = circle_area(67.875342)
+print "circle_area - " + str(circlearea)
+
+spherevolume = sphere_volume(5)
+print "sphere_volume - " + str(spherevolume) 
+spherevolume = sphere_volume(66666777.345)
+print "sphere_volume - " + str(spherevolume) 
+
+avg_ = avg_volume(10, 20)
+print "avg_volume - " + str(avg_)
+avg_ = avg_volume(5, 24)
+print "avg_volume - " + str(avg_)
+
+area_ = area(1, 2, 2.5)
+print area_
+area_ = area(0.1, 2, 56)
+print area_
+
+rightalign = right_align("Hello")
+print rightalign
+rightalign = right_align("Goodbye")
+print rightalign
+
+center_ = center("Hello")
+print center_
+center_ = center("Goodbye")
+print center_
+
+box_hello = msg_box("Hello")
+print box_hello
+box_hello = msg_box("Goodbye")
+print box_hello
+	
+box_cats = msg_box("I like cats!")
+print box_cats
+box_cats = msg_box("I hate cats!")
+print box_cats
--- madlib.py
+++
@@ -1,3 +0,0 @@
-def output(noun, subject, verb)
-	return"""
-
