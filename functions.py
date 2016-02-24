b584103b3a7e0f3fdc8fe35426d3472eea6a6fb9
cmis <cmis@cmis-hs20.(none)>
2016-02-24 10:09:57 +0700

    I have finished everything with swag as usual.

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import math
+
 def add(a, b):
 	return a + b
 def sub(a, b):
@@ -5,9 +7,9 @@ def sub(a, b):
 def mul(a, b):
 	return a * b
 def div(a, b):
-	return a / b
+	return float(a) / float(b)
 
-a = add(9.1, 8)
+a = add(9.1, 8.3)
 b = sub(9.1, 8)
 c = mul(9.1, 8)
 d = div(9.1, 8)
@@ -44,11 +46,11 @@ def sphere_volume(radius):
 spherevolume = sphere_volume(5)
 print "sphere_volume - " + str(spherevolume) 
 
-def avg_volume(x, y):
-	a = x / 2
-	b = y / 2
-	a_volume = sphere_volume(a)
-	b_volume = sphere_volume(b)
+def avg_volume(diameter_1, diameter_2):
+	radius_a = diameter_1 / 2
+	radius_b = diameter_2 / 2
+	a_volume = sphere_volume(radius_a)
+	b_volume = sphere_volume(radius_b)
 	together = a_volume + b_volume
 	avg = together / 2.0
 	return avg
@@ -63,8 +65,8 @@ def area(a, b, c):
 	sub_b = s - b
 	sub_c = s - c
 	mul_eve = s * sub_a * sub_b * sub_c
-	sqrt = math.sqrt(mul_eve)
-	return sqrt
+	sqrt_ = math.sqrt(mul_eve)
+	return sqrt_
 
 area_ = area(1, 2, 2.5)
 print area_
@@ -103,60 +105,61 @@ print box_hello
 box_cats = msg_box("I like cats!")
 print box_cats
 
-add = add(9.1, 8)
-print add
-add = add(89.9, 98)
-print add
 
-sub = sub(9.1, 8)
-print sub
-sub = sub(91, 86)
-print sub
+add__ = add(9.1, 8)
+print msg_box(str(add__))
+add_ = add(9.2, 9)
+print msg_box(str(add_))
 
-mul = mul(9.1, 8)
-print mul
-mul = mul(41, 85)
-print mul
+sub__ = sub(9.1, 8)
+print msg_box(str(sub__))
+sub_ = sub(91, 86)
+print msg_box(str(sub_))
 
-div = div(9.1, 8)
-print div
-div = div(1, 8.7)
-print div
+mul__ = mul(9.1, 8)
+print msg_box(str(mul__))
+mul_ = mul(41, 85)
+print msg_box(str(mul_))
+
+div__ = div(9, 8)
+print msg_box(str(div__))
+div_ = div(1, 8)
+print msg_box(str(div_))
 
 hours = hours_from_seconds(86400)
-print "hours_from_seconds - " + str(hours)
+print msg_box("hours_from_seconds - " + str(hours))
 hours = hours_from_seconds(565656565656565)
-print "hours_from_seconds - " + str(hours)
+print msg_box("hours_from_seconds - " + str(hours))
 
 circlearea = circle_area(5)
-print "circle_area - " + str(circlearea)
+print msg_box("circle_area - " + str(circlearea))
 circlearea = circle_area(67.875342)
-print "circle_area - " + str(circlearea)
+print msg_box("circle_area - " + str(circlearea))
 
 spherevolume = sphere_volume(5)
-print "sphere_volume - " + str(spherevolume) 
+print msg_box("sphere_volume - " + str(spherevolume)) 
 spherevolume = sphere_volume(66666777.345)
-print "sphere_volume - " + str(spherevolume) 
+print msg_box("sphere_volume - " + str(spherevolume))
 
 avg_ = avg_volume(10, 20)
-print "avg_volume - " + str(avg_)
+print msg_box("avg_volume - " + str(avg_))
 avg_ = avg_volume(5, 24)
-print "avg_volume - " + str(avg_)
+print msg_box("avg_volume - " + str(avg_))
 
 area_ = area(1, 2, 2.5)
-print area_
-area_ = area(0.1, 2, 56)
-print area_
+print msg_box(str(area_))
+area_ = area(10, 20, 23)
+print msg_box(str(area_))
 
 rightalign = right_align("Hello")
-print rightalign
+print msg_box(rightalign)
 rightalign = right_align("Goodbye")
-print rightalign
+print msg_box(rightalign)
 
 center_ = center("Hello")
-print center_
+print msg_box(center_)
 center_ = center("Goodbye")
-print center_
+print msg_box(center_)
 
 box_hello = msg_box("Hello")
 print box_hello
