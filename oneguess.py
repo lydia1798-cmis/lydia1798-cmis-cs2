037e8a30e74bffec1024c5fd58a9b018ac1e4528
cmis <cmis@cmis-hs19.(none)>
2016-03-10 14:35:25 +0700

    I think I get it

---
+++ oneguess.py
@@ -0,0 +1,12 @@
+import random
+
+def THEnumber(minimum, maximum):
+	range_= random.radiant(minimum, maximum)
+	return range_
+
+def output()
+
+def main():
+	minimum = int(raw_input("What is the minimum number?"))
+	maximum = int(raw_input("What is the maximum number?"))
+	Thinking = int(raw_input("
