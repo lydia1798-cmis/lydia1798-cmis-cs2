cc78a1b9c1d3c14fa03371941015276970b239b9
cmis <cmis@cmis-hs19.(none)>
2016-03-15 09:55:39 +0700

    I thik I can do it

--- oneguess.py
+++ oneguess.py
@@ -1,12 +1,49 @@
 import random
+import math
 
-def THEnumber(minimum, maximum):
-	range_= random.radiant(minimum, maximum)
-	return range_
+def THEtarget(minimum, maximum):
+	target = random.radiant(minimum, maximum)
+	return target
 
-def output()
 
+def less(target, your_number):
+	if target < your_number
+		sub = your_number - target 
+		return """
+		Your target was {}.
+		Your guess was {}.
+		That's over by {}.
+		""".format(target, your_number, sub)
+	 
 def main():
 	minimum = int(raw_input("What is the minimum number?"))
 	maximum = int(raw_input("What is the maximum number?"))
-	Thinking = int(raw_input("
+	print "I'm thinking of a number from", minimum, "to", maximum, "."
+	target = THEnumber(minimum, maximum)
+	your_number = int(raw_input("What do you think it is?: "))
+	if target == your_number 
+		return """
+		Your target was {}.
+		Your guess was {}.
+		That's correct! You must be psychic!
+		""".format(target, your_number)
+	elif target > your_number
+		sub = target - your_number
+		return """
+		Your target was {}.
+		Your guess was {}.
+		That's under by {}.
+		""".format(target, your_number, sub)
+	elif target < your_number
+		sub = your_number - target 
+		return """
+		Your target was {}.
+		Your guess was {}.
+		That's over by {}.
+		""".format(target, your_number, sub)
+
+
+main()
+	
+	
+
