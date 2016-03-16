a6be276eaec2cb3fd34585e2aa3fbb1c74843906
cmis <cmis@cmis-hs19.(none)>
2016-03-16 11:12:43 +0700

    I finished it

--- oneguess.py
+++ oneguess.py
@@ -1,49 +1,35 @@
 import random
-import math
 
 def THEtarget(minimum, maximum):
-	target = random.radiant(minimum, maximum)
+	target = random.randint(minimum, maximum)
 	return target
-
-
-def less(target, your_number):
-	if target < your_number
-		sub = your_number - target 
-		return """
-		Your target was {}.
-		Your guess was {}.
-		That's over by {}.
-		""".format(target, your_number, sub)
 	 
 def main():
-	minimum = int(raw_input("What is the minimum number?"))
-	maximum = int(raw_input("What is the maximum number?"))
+	minimum = int(raw_input("What is the minimum number?: "))
+	maximum = int(raw_input("What is the maximum number?: "))
 	print "I'm thinking of a number from", minimum, "to", maximum, "."
-	target = THEnumber(minimum, maximum)
+	target = THEtarget(minimum, maximum)
 	your_number = int(raw_input("What do you think it is?: "))
-	if target == your_number 
-		return """
-		Your target was {}.
-		Your guess was {}.
-		That's correct! You must be psychic!
+	if target == your_number: 
+		print """
+Your target was {}.
+Your guess was {}.
+That's correct! You must be psychic!
 		""".format(target, your_number)
-	elif target > your_number
+	elif target > your_number:
 		sub = target - your_number
-		return """
-		Your target was {}.
-		Your guess was {}.
-		That's under by {}.
+		print """
+Your target was {}.
+Your guess was {}.
+That's under by {}.
 		""".format(target, your_number, sub)
-	elif target < your_number
+	elif target < your_number:
 		sub = your_number - target 
-		return """
-		Your target was {}.
-		Your guess was {}.
-		That's over by {}.
+		print """
+Your target was {}.
+Your guess was {}.
+That's over by {}.
 		""".format(target, your_number, sub)
 
 
-main()
-	
-	
-
+main()	
