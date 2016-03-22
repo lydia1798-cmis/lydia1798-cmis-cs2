5377025b19a8f964b58ab7d0a38f1f28e2f2246f
cmis <cmis@cmis-hs06.(none)>
2016-03-22 09:55:19 +0700

    I am getting there

--- simple_program.py
+++ simple_program.py
@@ -1,38 +1,25 @@
 import math
 
-n = 2
-
-def add(a, b):
-	c = a + b
-	return c
-
-def sub(x_1, x_2):
-	a = x_1 - x_2
-	return a
-
-def pow_(a):
-	math.pow(a, n)
-	return a**n
-
-def sqrt(b):
-	b_sqrt = math.sqrt(b)
-	return b_sqrt
+def distance(x_1, y_1, x_2, y_2):
+	x = abs(x_2 - x_1)
+	y = abs(y_2 - y_1)
+	x_sqrd = x ** 2
+	y_sqrd = y ** 2
+	add = x_sqrd + y_sqrd
+	sqrt = math.sqrt(add)
+	return sqrt
+	
 
 def output(c):
 	return "The distance between your two coordinate pairs: {}".format(c)
 
 def main():
-	x_1 = raw_input("Type the x in the first coordinate pair: ")
-	y_1 = raw_input("Type the y in the first coordinate pair: ")
-	x_2 = raw_input("Type the x in the second coordinate pair: ")
-	y_2 = raw_input("Type the y in the secind coordinate pair: ")
-	a = sub(float(x_1), float(x_2))
-	b = sub(float(y_1), float(y_2))
-	c = pow_(float(a))
-	d = pow_(float(b))
-	sum_ = add(float(c), float(d))
-	sqrt_ = sqrt(float(sum_))
-	print output(sqrt_)
+	x_1 = float(raw_input("Type the x in the first coordinate pair: "))
+	y_1 = float(raw_input("Type the y in the first coordinate pair: "))
+	x_2 = float(raw_input("Type the x in the second coordinate pair: "))
+	y_2 = float(raw_input("Type the y in the secind coordinate pair: "))
+	TheDistance = distance(x_1, y_1, x_2, y_2)
+	print output(TheDistance)
 
 main()
 
