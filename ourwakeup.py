31c971c66720e68001688cc6b82b84f714a75aae
cmis <cmis@cmis-hs01.(none)>
2016-04-18 09:54:17 +0700

    we need to figure out the out of range thing

---
+++ ourwakeup.py
@@ -0,0 +1,55 @@
+import math
+
+def average_(n0, n1, n2, n3, n4, numberofnumbers):
+	sum_ = n0 + n1 + n2 + n3 + n4
+	average = sum_ / number_correct
+	return average
+
+number_correct = 0
+#to find the number of numbers for the average
+	
+#between 0 and 10
+def range_(number):
+	if number >= 0 and number < 10:
+		number_correct = number_correct + 1
+	else:
+		return "{} is out of range.".format(number)
+
+# e = even o = odd
+def O_E(average):
+	int_average = int(average)
+	if int_average % 2 == 0:
+		return even
+	else:
+		return odd
+
+def output():
+	print """
+The average is {}.
+The integer part of the average is {}.
+The integer part is {}.""".format(average, int(average), e_o)
+
+def main():
+	print """
+
+This  program will ask you for 5 integer or float values.
+It will calculate the average of all valus from 0 inclusive to 10 exclusive.
+It will print out whether the resulting average is even or odd.
+
+"""
+	n_0 = float(raw_input("n0: "))
+	n0 = range_(n_0)
+	n_1 = float(raw_input("n1: "))
+	n1 = range_(n_1)
+	n_2 = float(raw_input("n2: "))
+	n2 = range_(n_2)
+	n_3 = float(raw_input("n3: "))
+	n3 = range_(n_3)
+	n_4 = float(raw_input("n4: "))
+	n4 = range_(n_4)
+	average = average(n0, n1, n2, n3, n4)
+	o_e = O_E(average)
+	print output
+
+main()
+	
