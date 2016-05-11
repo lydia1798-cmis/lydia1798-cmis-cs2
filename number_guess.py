cd79549b9c6a75ad87deda6f5c2def123f5f278f
cmis <cmis@cmis-hs06.(none)>
2016-05-11 09:54:58 +0700

    I can't with this bonus...

--- number_guess.py
+++ number_guess.py
@@ -16,36 +16,42 @@ def rounds(right_n, number_recursed, uc):
 		print "too low"
 		return rounds(right_n, number_recursed - 1, uc)
 
-def bonus(cc, n, number_recursed):
+def bonus(cc, computers_number, number_recursed):
 	y_number = raw_input("too (h)igh, too (l)ow, (c)orrect: ")
+	lowest_computers_number
 	if y_number == "c":
 		print "You're correct!"
-		return cc + 1
+		cc = cc + 1
+		return cc
+	elif number_recursed == 0:
+		return cc
+	elif y_number == "l":
+		lowest_computers_number = computers_number
+		computers_number = (lowest_computers_number + highest_computers_number / 2) + computers_number
+		print "I guess {}".format(computers_number)
+		bonus(cc, computers_number, number_recursed - 1)
 	elif y_number == "h":
-		n = (100 + n) / 2
-		print "I guess {}".format(n)
-		bonus(cc, n, number_recursed - 1)
-	else:
-		half_of = (100 - n)/2
-		n = n + half_of
-		print "I guess {}".format(n)
-		bonus(cc, n, number_recursed - 1)
+		highest_computers_number = computers_number 
+		computers_number = lowest_computers_number + highest_computers_number/ 2
+		print "I guess {}".format(computers_number)
+		bonus(cc, computers_number, number_recursed - 1)
 
-def round_number(n, uc):	
+def round_number(n, uc, cc):	
 	if n == 0:
 		print """
 {} {}
-""".format(str(uc, cc))
+""".format(str(uc), str(cc))
 	else:
-		right_n = random.randint(0, 100)
+		right_n = 3
 		print "\nYou have {} rounds left".format(n)
 		uc = rounds(right_n, 4, uc)
 		print "\nI guess 50"
-		n = 50
+		computers_number = 50
+
 		cc = bonus(0, 50, 4)
-		round_number(n - 1, uc)
+		round_number(n - 1, uc, cc)
 	
 def main():
-	round_number(3, 0)
+	round_number(3, 0, 0)
 
 main()
