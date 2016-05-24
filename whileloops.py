6ef01450d26ca969c62b51f6db61c308e56e7bfd
cmis <cmis@cmis-hs06.(none)>
2016-05-24 09:55:22 +0700

    okay then

---
+++ whileloops.py
@@ -0,0 +1,20 @@
+#while x < 100:
+	#print x
+	#x += 1
+
+#def count_down(x):
+	#while x >= 0:
+		#print x 
+		#x -= 1
+
+#def countdown(x):
+	#if x > 0:
+		#while x >= 0:
+			#print x
+			#x -= 1
+	#else:
+		#while x <= 0:
+		#	print x
+		#	x += 1
+
+
