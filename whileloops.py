72bf086a515b4be5c3cc71fee49794d5c0055d9e
cmis <cmis@cmis-hs06.(none)>
2016-05-30 09:57:33 +0700

    almost:

--- whileloops.py
+++ whileloops.py
@@ -30,14 +30,33 @@ def countFrom2(x, y):
 def sumOfOdds(n):
 	sumOfodds = 0
 	if n > 0:
-		while n != 0:
+		while n >= 0:
 			if n%2 != 0:
-				sumOfodds += 1
+				sumOfodds += n
 			n -= 1
+	else:
+		while n <= 0:
+			if n%2 != 0:
+				sumOfodds += n
+			n += 1
+	return sumOfodds
+
+def grid(w, h):
+	out = ""
+	while h > 0:
+		w = h
+		while w != 0:
+			out += "!"
+			w -= 1
+		out += "\n"	
+		h -= 1
+	return out
+	
+	
 			
 
 def main():
-	print sumOfOdds(10)
+	print grid(5, 10)
 
 main()
 
